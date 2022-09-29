eb68ed0fb5fea805882ad154095102141c4c07f1
leandrofiadone <leanfiadone@gmail.com>
2022-09-29 17:21:39 -0300

    skill fix

--- pages/api/getSkills.ts
+++ pages/api/getSkills.ts
@@ -15,6 +15,6 @@ export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse<Data>
 ) {
-	const skills: Skill[] = await sanityClient.fetch(query);
+	const skills: Skill = await sanityClient.fetch(query);
 	res.status(200).json({ skills });
 }
