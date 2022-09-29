95a13ca8cabd707436c809200ca1eb7be6a3983d
leandrofiadone <leanfiadone@gmail.com>
2022-09-29 20:08:16 -0300

    morefixdeploy

--- sanity.ts
+++ sanity.ts
@@ -5,7 +5,7 @@ export const config = {
 	dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
 	projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
 	apiVersion: "2021-03-25",
-	useCdn: process.env.NODE_ENV === "production",
+	useCdn: process.env.SANITY_NODE_ENV === "production",
 };
 
 export const sanityClient = createClient(config);
