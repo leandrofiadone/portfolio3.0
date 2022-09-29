7f21a0407a038a232963d79f30420d07c7dd5f3e
leandrofiadone <leanfiadone@gmail.com>
2022-09-29 17:16:35 -0300

    fix pageInfo

--- pages/api/getPageInfo.ts
+++ pages/api/getPageInfo.ts
@@ -13,6 +13,6 @@ export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse<Data>
 ) {
-	const pageInfo: PageInfo[] = await sanityClient.fetch(query);
+	const pageInfo: PageInfo = await sanityClient.fetch(query);
 	res.status(200).json({ pageInfo });
 }
