1eb966143390a19a018a8de02db52e05ab4b4182
leandrofiadone <leanfiadone@gmail.com>
2022-10-17 16:52:36 -0300

    moreresponsive

--- components/ProjectExperience.tsx
+++ components/ProjectExperience.tsx
@@ -13,13 +13,13 @@ const ProjectExperience = ({ projects }: Props) => {
 		flex-col text-lft md:flex-row max-w-full  
 		justify-evenly mx-auto items-center">
 			<h3
-				className="absolute top-20 uppercase 
-			tracking-[20px] text-gray-500 xl:text-2xl md:text-lg">
+				className="absolute sm:top-20 top-14 uppercase 
+			sm:tracking-[20px] tracking-[10px] text-gray-500 xl:text-2xl md:text-lg  text-sm">
 				projects
 			</h3>
 
 			<div
-				className="w-full flex space-x-5 overflow-x-scroll 
+				className="w-full xl:mt-5 sm:mt-20 flex space-x-5 overflow-x-scroll 
 			scroll-smooth hover:scroll-auto p-10 snap-x snap-mandatory
 			scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 z-10">
 				{projects?.map((project) => (
