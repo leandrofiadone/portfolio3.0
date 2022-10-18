723c4a964f04347736a53c890b304bc92ba659d4
leandrofiadone <leanfiadone@gmail.com>
2022-10-18 20:21:54 -0300

    projectLink

--- components/ProjectExperienceCard.tsx
+++ components/ProjectExperienceCard.tsx
@@ -14,23 +14,25 @@ const ProjectExperienceCard = ({ project }: Props) => {
 			className="flex flex-col rounded-lg xl:mt-0 md:mt-10 mt-8 items-center sm:space-y-4 space-y-1 flex-shrink-0
         w-[240px] md:w-[400px] lg:w-[300px]  xl:w-[520px] snap-center bg-[#292929] 
 		p-1 hover:opacity-100 opacity-40  transition-opacity duration-200 shadow-lg shadow-cyan-500/50">
-			<motion.img
-				initial={{
-					y: -100,
-					opacity: 0,
-				}}
-				transition={{
-					duration: 1.2,
-				}}
-				whileInView={{
-					opacity: 1,
-					y: 0,
-				}}
-				viewport={{ once: true }}
-				className="w-96 lg:w-[300px] xl:w-[500px]"
-				src={urlFor(project?.projectImage).url()}
-				alt="BarberApp"
-			/>
+			<a href={project?.nameLink}>
+				<motion.img
+					initial={{
+						y: -100,
+						opacity: 0,
+					}}
+					transition={{
+						duration: 1.2,
+					}}
+					whileInView={{
+						opacity: 1,
+						y: 0,
+					}}
+					viewport={{ once: true }}
+					className="w-96 lg:w-[300px] xl:w-[500px]"
+					src={urlFor(project?.projectImage).url()}
+					alt="BarberApp"
+				/>
+			</a>
 
 			<div className="px-0 xl:px-10 ">
 				<a href={project?.nameLink}>
