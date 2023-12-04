495d8e81e30416fcdced4be0dd22cf7c3f8757a6
Leandro Fiadone <leanfiadone@gmail.com>
2023-12-04 09:04:24 -0300

    cambios BackGround Skills Diciembre 2023

--- components/About.tsx
+++ components/About.tsx
@@ -1,55 +1,79 @@
-import { motion } from "framer-motion";
-import Image from "next/image";
-import React from "react";
-import { urlFor } from "../sanity";
-import { PageInfo } from "../typings";
+import {motion} from "framer-motion"
+import Image from "next/image"
+import React from "react"
+import {urlFor} from "../sanity"
+import {PageInfo} from "../typings"
 
 type Props = {
-	pageInfo: PageInfo;
-};
-
-function About({ pageInfo }: Props) {
-	return (
-		<div className="transform skew-y-6 bg-[#005248] bg-gradient-to-r from-slate-900 via-cyan-900 to-slate-900 backdrop-opacity-50 ">
-			<motion.div
-				initial={{ opacity: 0 }}
-				whileInView={{ opacity: 1 }}
-				transition={{ duration: 1.5 }}
-				className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl xl:px-10 lg:px-5 md:px-8 px-2 justify-evenly mx-auto items-center -skew-y-6">
-				<h3 className="absolute top-14 md:top-20 uppercase tracking-[20px] text-[#F7AB0A]  md:text-xl text-base font-['Electrolize'] ">
-					About
-				</h3>
-				<motion.img
-					initial={{
-						x: -200,
-						opacity: 0,
-					}}
-					whileInView={{ opacity: 1, x: 0 }}
-					src={urlFor(pageInfo?.aboutMePic).url()}
-					alt="fotoabout"
-					width="500px"
-					height="500px"
-					transition={{
-						duration: 0.7,
-					}}
-					viewport={{ once: true }}
-					className="-mb-20 md:mb-0 flex-shrink-0 w-40 h-40  mt-10 rounded-full object-cover
-        md:rounded-[32px] md:w-64 sm:h-80 xl:w-[500px] xl:h-[600px] bg-cyan-500 shadow-lg shadow-cyan-500/50 "
-				/>
-
-				<div className="md:space-y-5 pt-10 space-y-4 px-0 md:px-10 font-['Electrolize'] ">
-					<h4 className="text-sm md:text-2xl xl:text-4xl text-lg font-semibold ">
-						Here is a{" "}
-						<span className="underline decoration-[#F7AB0A] ">little</span>{" "}
-						background
-					</h4>
-					<p className="md:text-sm text-xs lg:text-base xl:text-lg ">
-						{pageInfo?.backgroundInformation}
-					</p>
-				</div>
-			</motion.div>
-		</div>
-	);
+  pageInfo: PageInfo
 }
 
-export default About;
+function About({pageInfo}: Props) {
+  return (
+    <div className="transform skew-y-6 bg-[#005248] bg-gradient-to-r from-slate-900 via-cyan-900 to-slate-900 backdrop-opacity-50 ">
+      <motion.div
+        initial={{opacity: 0}}
+        whileInView={{opacity: 1}}
+        transition={{duration: 1.5}}
+        className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl xl:px-10 lg:px-5 md:px-8 px-2 justify-evenly mx-auto items-center -skew-y-6">
+        <h3 className="absolute top-14 md:top-20 uppercase tracking-[20px] text-[#F7AB0A]  md:text-xl text-base font-['Electrolize'] ">
+          About
+        </h3>
+        <motion.img
+          initial={{
+            x: -200,
+            opacity: 0
+          }}
+          whileInView={{opacity: 1, x: 0}}
+          src={urlFor(pageInfo?.aboutMePic).url()}
+          alt="fotoabout"
+          width="500px"
+          height="500px"
+          transition={{
+            duration: 0.7
+          }}
+          viewport={{once: true}}
+          className="-mb-20 md:mb-0 flex-shrink-0 w-40 h-40  mt-10 rounded-full object-cover
+        md:rounded-[32px] md:w-64 sm:h-80 xl:w-[500px] xl:h-[600px] bg-cyan-500 shadow-lg shadow-cyan-500/50 "
+        />
+
+        <div className="md:space-y-5 pt-10 space-y-4 px-0 md:px-10 font-['Electrolize'] ">
+          <h4 className="text-sm md:text-2xl xl:text-4xl text-lg font-semibold ">
+            Here is a{" "}
+            <span className="underline decoration-[#F7AB0A] ">little</span>{" "}
+            background
+          </h4>
+          <p className="md:text-sm text-xs lg:text-base xl:text-lg ">
+            📑 Since March 2023, I've been a Full-Stack Developer with a strong
+            Front-End focus at{" "}
+            <a
+              href="https://pl4nner.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-yellow-500 underline hover:no-underline">
+              Pl4nner.com
+            </a>
+            , leveraging my +800 hours of dedicated study at{" "}
+            <a
+              href="https://www.soyhenry.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-yellow-500 underline hover:no-underline">
+              SoyHenry.com
+            </a>
+            . While I specialize in React, Next.js, Node.js, and MongoDB for
+            front-end development, I'm also deeply interested in AI and
+            Blockchain development. This fascination drives my continuous
+            exploration and learning in these domains. My foundation in design
+            principles and audiovisual production complements my passion for
+            creating intuitive front-end solutions. I'm committed to evolving my
+            skills, staying updated with emerging technologies, and contributing
+            innovative ideas to every project! 💻📚🌍
+          </p>
+        </div>
+      </motion.div>
+    </div>
+  )
+}
+
+export default About
--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,29 +1,35 @@
-import React from "react";
-import { motion } from "framer-motion";
-import { Skill } from "../typings";
-import { urlFor } from "../sanity";
+import React from "react"
+import {motion} from "framer-motion"
+import {Skill} from "../typings"
+import {urlFor} from "../sanity"
 
 type Props = {
-	skill: Skill;
-	directionLeft?: boolean;
-};
+  skill: Skill
+  directionLeft?: boolean
+}
 
-const Skill = ({ skill, directionLeft }: Props) => {
-	return (
-		<div className="group relative flex cursor-pointer">
-			<motion.img
-				src={urlFor(skill?.image).url()}
-				className="rounded-full border border-gray-500 object-cover w-14 md:w-16 xl:w-24 filter group-hover:grayscale transition duration-300 ease-in-out"
-			/>
-			<div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white rounded-full w-20 md:w-22 xl:w-24  z-0">
-				<div className="flex items-center justify-center h-full">
-					<p className="md:text-3xl text-sm font-bold text-black opacity-100">
-						{skill?.progress}%
-					</p>
-				</div>
-			</div>
-		</div>
-	);
-};
+const Skill = ({skill, directionLeft}: Props) => {
+  return (
+    <div className="group relative flex cursor-pointer">
+      <motion.img
+        whileHover={{
+          scale: 1.2,
+          transition: {duration: 0.1} // Ajusta la duración aquí
+        }}
+        src={urlFor(skill?.image).url()}
+        className=" border-gray-500 object-cover w-14 md:w-16 xl:w-24 filter group-hover:grayscale transition duration-300 ease-in-out"
+      />
+      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out w-20 md:w-22 xl:w-24  z-0">
+        <div className="flex items-center justify-center h-full">
+          <p
+            className="md:text-xl text-sm font-bold text-white opacity-100"
+            style={{textShadow: "2px 2px 6px rgba(0,0,0,2)"}}>
+            {skill?.title}
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
 
-export default Skill;
+export default Skill
